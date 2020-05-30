643d77d2b2af591ebbbaaf0a1c8478abe04ea1de
Wátila Costa <watilafreire26@gmail.com>
2020-05-30 19:24:57 -0300

    atualizando

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,6 @@ const Api = axios.create({
   baseURL: "http://localhost:3001/api/v1"
 })
 
-// Este algoritmo intercepta as requisições 
-// para a Api e envia o headers com o token
 Api.interceptors.request.use(async config => {
   const token = getToken()
   
